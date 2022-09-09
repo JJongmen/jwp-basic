9fcef3732d8900fff702987708a5ee09d03a4c5d
JJongmen <pjy90123@gmail.com>
2022-09-09 23:26:07 +0900

    람다를 활용하여 리팩토링

--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -12,28 +12,44 @@ public class UserDao {
 
     public void insert(User user) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", user.getUserId(),
-                user.getPassword(), user.getName(), user.getEmail());
+//        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", user.getUserId(),
+//                user.getPassword(), user.getName(), user.getEmail());
+        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", pstmt -> {
+            pstmt.setString(1, user.getUserId());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+        });
     }
 
     public void update(User user) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        jdbcTemplate.update("UPDATE USERS SET password=?, name=?, email=? WHERE userId=?",
-                user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+//        jdbcTemplate.update("UPDATE USERS SET password=?, name=?, email=? WHERE userId=?",
+//                user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+        jdbcTemplate.update("UPDATE USERS SET password=?, name=?, email=? WHERE userId=?", pstmt -> {
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+        });
     }
 
     public List<User> findAll() {
         JdbcTemplate<User> jdbcTemplate = new JdbcTemplate<>();
-        RowMapper<User> rowMapper = new RowMapper<User>() {
-            @Override
-            public User mapRow(ResultSet rs) throws SQLException {
-                User user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-                return user;
-            }
-        };
-
-        return jdbcTemplate.query("SELECT userId, password, name, email FROM USERS", rowMapper);
+//        RowMapper<User> rowMapper = new RowMapper<User>() {
+//            @Override
+//            public User mapRow(ResultSet rs) throws SQLException {
+//                User user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+//                        rs.getString("email"));
+//                return user;
+//            }
+//        };
+//
+//        return jdbcTemplate.query("SELECT userId, password, name, email FROM USERS", rowMapper);
+        return jdbcTemplate.query("SELECT userId, password, name, email FROM USERS", rs ->
+                new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email"))
+        );
     }
 
     public User findByUserId(String userId) {
@@ -48,6 +64,9 @@ public class UserDao {
             }
         };
 
-        return jdbcTemplate.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userid=?", rowMapper, userId);
+        return jdbcTemplate.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userid=?",
+                rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                        rs.getString("email")),
+                userId);
     }
 }
