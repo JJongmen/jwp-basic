c25e78a325ea8680b4ebab77139651f46a2e4e1a
JJongmen <pjy90123@gmail.com>
2022-09-13 21:10:58 +0900

    질문 목록 및 질문 보기 구현

---
+++ src/main/java/next/controller/ShowQuestionController.java
@@ -0,0 +1,21 @@
+package next.controller;
+
+
+import core.mvc.Controller;
+import next.dao.QuestionDao;
+import next.model.Question;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ShowQuestionController implements Controller {
+
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String questionId = req.getParameter("questionId");
+        QuestionDao questionDao = new QuestionDao();
+        Question question = questionDao.findByQuestionId(Integer.parseInt(questionId));
+        req.setAttribute("question", question);
+        return "/qna/show.jsp";
+    }
+}
